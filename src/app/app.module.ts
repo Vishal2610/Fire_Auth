04a6c2b528426e2a23716c0adf38da492ba3a9e8
Sai Vishal Tyagadurgam <styagadu@opentext.com>
2017-10-23 11:21:30 +0530

    google login'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,19 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import * as firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyC-Btw9ZGKTDHjddRY4P-W2-IVhCLSwjCo",
+  authDomain: "auth-49c2f.firebaseapp.com",
+  databaseURL: "https://auth-49c2f.firebaseio.com",
+  projectId: "auth-49c2f",
+  storageBucket: "auth-49c2f.appspot.com",
+  messagingSenderId: "583702136096"
+};
+
+firebase.initializeApp(config);
+
 @NgModule({
   declarations: [
     MyApp,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import firebase from 'firebase';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +11,50 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  provider = new firebase.auth.GoogleAuthProvider;
+
+  email: string = 'vishal.tdm@gmail.com';
+  password: string = 'vishal123';
+
+  login(){
+    firebase.auth().createUserWithEmailAndPassword(this.email , this.password).then((success)=>{
+      console.log(success);
+      
+    }).catch((err)=>{
+      console.log("error");
+      
+      console.log(err);
+      
+    });
+  }
+
+  googlelogin(){
+    firebase.auth().signInWithPopup(this.provider);
+    firebase.auth().getRedirectResult().then((success)=>{
+      console.log(success);
+      
+    }).catch((err)=>{
+      console.log("error");
+      
+      console.log(err);
+      
+    });
+  }
+
+  logout(){
+    firebase.auth().signOut().then((success)=>{
+      console.log("logout");
+      
+      console.log(success);
+      
+    }).catch((err)=>{
+      console.log("err");
+      
+      console.log(err);
+      
+    });
   }
 
 }
